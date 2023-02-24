4fe867256bf9396d3e95b7e623b7ab8dfe8c1454
Jade Bell <bellj22@nku.edu>
2023-02-24 08:58:49 -0500

    JavaScript File

---
+++ JavascriptFile.js
@@ -0,0 +1,9 @@
+function submitButton() {
+	let webpage = document.getElementById("SB");
+}
+
+function largeNumber() {
+	let max = [1,2,3,4,5,6,7];
+	// find largest number in list
+	return Math.max(max);
+}

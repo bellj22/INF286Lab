330eb53313a3cec431ac7c0626c6c2f2e9fbdd62
Jade Bell <bellj22@nku.edu>
2023-03-02 12:47:29 -0500

    Updated JS File 3/2

--- JavascriptFile.js
+++ JavascriptFile.js
@@ -7,3 +7,38 @@ function largeNumber() {
 	// find largest number in list
 	return Math.max(max);
 }
+
+function htjs() {
+	let xhr = new XMLHttpRequest();
+
+	// 2. Configure it w/ GET request
+	xhr.open('GET', https://api.weather.gov/gridpoints/TOP/31,80/forecast');
+
+	xhr.responseType = 'json';
+	xhr.send();
+
+	xhr.onload= function(); {
+		let repsonseObj= xhr.response;
+		alert(responseObj.message);
+
+	xhr.onload = function() {
+		if (xhr.status !- 200) {
+			alert(`Error ${xhr.status}: ${xhr.statusText}`);
+		} else {
+		alert(`Done, got $xhr.response.length} bytes`);
+		}
+	};
+
+	xhr.onprogress = function(event)
+	if (event.lengthComputable) {
+		alert(`Received ${event.loaded} of ${event.total} bytes`);
+	  } else {
+		alert(`Received ${event.loaded} bytes`); // no Content-Length
+	  }
+
+	};
+
+	xhr.onerror = function() {
+	  alert("Request failed");
+	};
+}
